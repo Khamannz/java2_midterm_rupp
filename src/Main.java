206e467a9d08e7200e7992afef23b0b7b10d0822
Kha Mann <Khamann@gmail.com>
2024-01-10 20:17:15 +0700

    ffff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import javax.security.sasl.RealmChoiceCallback;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Rectangle rectangle= new Rectangle(100.3,42.2);
+
+
+        System.out.println("rectangle: "+ rectangle.getArea());
+        System.out.println("rectangle: "+ rectangle.getPerimeter());
+
+
+        Square square= new Square(4.0);
+
+        System.out.println("square: "+ square.getArea());
+        System.out.println("square: "+ square.getPerimeter());
+
+
+
     }
 }
