876ef1df9655911545ddfba6bcb36a0a8d2dcda9
Kha Mann <Khamann@gmail.com>
2024-01-10 20:07:56 +0700

    f

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -5,12 +5,12 @@ public abstract class Rectangle {
     Double width=20.0;
     Double height= 200.0;
 
-    public Rectangle(Double width, Double height, Double p, Double l) {
-        this.width = width;
-        this.height = height;
-        P = p;
-        L = l;
-    }
+//    public Rectangle(Double width, Double height, Double p, Double l) {
+//        this.width = width;
+//        this.height = height;
+//        P = p;
+//        L = l;
+//    }
 
     Double P= 32.0;
     Double L= 100.0;
